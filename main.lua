af8c9f13f004617a08a5600d069d314a653fc4c6
Mr.Lee <limj0825@foxmail.com>
2020-11-02 16:45:34 +0800

    fix bug

--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@ local exec = require("common/osexec").exec
 local function loadDir(bot, dir)
   local files = exec("ls "..dir)
   for k, v in ipairs(files) do
-    print("加载"..v)
+    os.execute("echo '加载"..v.."'' > log")
     local app = require(dir.."/"..v:gsub("%.lua", ""))
     for i = 1, #app.event do
-     bot:subscribe(app.event[i], app.run)
-     print("绑定事件"..app.event[i])
+      bot:subscribe(app.event[i], app.run)
+      os.execute("echo '绑定事件"..app.event[i]..". > log")
     end
   end
 end
@@ -16,6 +16,7 @@ local function onLoad(bot)
   loadDir(bot, "groupMessage")
 end
 
+os.execute("> log")
 local config = require("config")
 local bot = Bot(config.Username, config.Password, "device.json")
 bot:login()
